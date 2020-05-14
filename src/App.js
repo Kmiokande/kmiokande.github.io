bea5518c3042fc812e8e22322052d40a9469d6bc
Kmiokande <brunnodesouzabezerra@gmail.com>
2020-05-14 16:29:30 -0300

    Pequenas alterações no profile

--- src/App.js
+++ src/App.js
@@ -13,12 +13,12 @@ function App() {
         <div className="profile-card__cnt js-profile-cnt">
           <div className="profile-card__name">Bruno de Souza Bezerra</div>
           <div className="profile-card__txt">
-            Desenvolvedor <strong>Python</strong>
+            Desenvolvedor <strong>Python</strong> e <strong>Android</strong>
             <p>Atualmente cursando Bacharelado em Sistemas de Informação pela
               Universidade Federal do Rio Grande do Norte - UFRN (2017-2021). Tecnólogo
 				      em Gestão da Tecnologia da Informação pela UNIP (2012-2015).
-				      Tenho experiência com Python, API Rest, Desenvolvimento Android,
-				      IoT, Banco de Dados, e agora, me metendo com React e React Native.</p>
+				      Tenho experiência com Python, API Rest, Desenvolvimento Android e
+				      IoT.</p>
           </div>
           <div class="profile-card-social">
             <a href="https://github.com/Kmiokande" className="profile-card-social__item">
